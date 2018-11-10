f6375074273decaa52c939141406b2d47e778a29
John Pastor <johnpastor90@gmail.com>
2018-11-10 00:20:55 -0600

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mystery Meat Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bison Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Capybara Burger', false);
